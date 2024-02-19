bfae4e1e512897584f5dbaa994af719714dce4f6
Nicolas Pereira <nicolas.pp333@gmail.com>
2024-02-19 16:37:00 -0300

    Day 21

---
+++ day20-21/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('Courier', 20, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.hideturtle()
+        self.color("white")
+        self.goto(0, 270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.update_scoreboard()
