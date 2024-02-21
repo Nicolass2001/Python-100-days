853722ca8c5af28652f1f7c0d292c0501995b6f1
Nicolas Pereira <nicolas.pp333@gmail.com>
2024-02-21 16:30:05 -0300

    Day 23

--- day23/main.py
+++ day23/main.py
@@ -3,12 +3,36 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
+import random
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    if random.randint(1,6) == 1:
+        car_manager.generate_car()
+        
+    car_manager.move_cars()
+
+    if car_manager.hit(player):
+        game_is_on = False
+        scoreboard.game_over()
+
+    if player.pass_level():
+        player.goto_starting_position()
+        scoreboard.add_level()
+        car_manager.increment_speed()
+
+screen.exitonclick()
